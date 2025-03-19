7b34d65638a2bc1beb5eac58032d457d150c8a65
Lakshmeshwarsoumya <lakshmeshwarsoumya1999@gmail.com>
2025-03-19 13:05:16 +0530

    done 3

--- JenkinsSampleProject/src/test/java/Pack2/OrgTest.java
+++ JenkinsSampleProject/src/test/java/Pack2/OrgTest.java
@@ -6,17 +6,17 @@ public class OrgTest {
 	@Test
 	public void createOrgTest() {
 
-		System.out.println("execute createOrgtest");
 		String URL = System.getProperty("url");
-		String Browser = System.getProperty("browser","Chrome");
+		String Browser = System.getProperty("browser","chrome");
 		String Username = System.getProperty("username");
 		String Password = System.getProperty("Password");
 
-		//System.out.println("execute createcontacttest");
+		
 		System.out.println(URL);
 		System.out.println(Browser);
 		System.out.println(Username);
 		System.out.println(Password);
+		System.out.println("execute createOrgTest");
 	}
 
 	@Test
--- JenkinsSampleProject/src/test/java/pack1/ContactTest.java
+++ JenkinsSampleProject/src/test/java/pack1/ContactTest.java
@@ -7,30 +7,24 @@ public class ContactTest {
 	public void createContactTest() {
 
 		String URL = System.getProperty("url");
-		String Browser = System.getProperty("browser","Chrome");
+		String Browser = System.getProperty("browser","chrome");
 		String Username = System.getProperty("username");
 		String Password = System.getProperty("Password");
 
-		System.out.println("execute createcontacttest");
+		
 		System.out.println(URL);
 		System.out.println(Browser);
 		System.out.println(Username);
 		System.out.println(Password);
+		System.out.println("execute createcontacttest");
 	}
 
 	@Test
 	public void modifyContactTest() {
 
 		System.out.println("execute modifycontacttest");
-		System.out.println("execute modifycontacttest");
-		System.out.println("execute modifycontacttest");
-		System.out.println("execute modifycontacttest");
-		System.out.println("execute modifycontacttest");
+		
 	}
-	@Test
-	public void deleteContactTest() {
-
-		System.out.println("execute deletecontacttest");
-	}
+	
 
 }
